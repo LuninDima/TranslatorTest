c5340687821721590e3cc0faf77e5dc3bcd74fbb
moondi <moon-di-2009@yandex.ru>
2022-07-17 22:16:57 +0300

    исправление ошибки (не отображался английский вариант слова).

--- app/src/main/java/com/example/translatortest/view/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/translatortest/view/main/adapter/MainAdapter.kt
@@ -7,14 +7,17 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.translatortest.R
 import com.example.translatortest.model.data.DataModel
+import org.w3c.dom.Text
 
 
 class MainAdapter(
     private var onListItemClickListener: OnListItemClickListener,
-    private var data: List<DataModel>): RecyclerView.Adapter<MainAdapter.RecyclerItemViewHolder>(){
+    private var data: List<DataModel>
+) : RecyclerView.Adapter<MainAdapter.RecyclerItemViewHolder>() {
 
-    fun setData(Data:List<DataModel>){
+    fun setData(data: List<DataModel>) {
         this.data = data
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerItemViewHolder {
@@ -25,7 +28,7 @@ class MainAdapter(
     }
 
 
-    override fun onBindViewHolder(holder:RecyclerItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerItemViewHolder, position: Int) {
         holder.bind(data.get(position))
     }
 
@@ -33,21 +36,22 @@ class MainAdapter(
         return data.size
     }
 
-    inner class RecyclerItemViewHolder(view: View): RecyclerView.ViewHolder(view){
-        fun bind(data: DataModel){
-            if(layoutPosition !=RecyclerView.NO_POSITION){
-                itemView.findViewById<TextView>(R.id.header_textview_recycler_item).text =
+    inner class RecyclerItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(data: DataModel) {
+            if (layoutPosition != RecyclerView.NO_POSITION) {
+                itemView.findViewById<TextView>(R.id.header_textview_recycler_item).text = data.text
+                itemView.findViewById<TextView>(R.id.description_textview_recycler_item).text =
                     data.meanings?.get(0)?.translation?.translation
-                itemView.setOnClickListener{openInNewWindow(data)}
+                itemView.setOnClickListener { openInNewWindow(data) }
             }
         }
     }
 
-    private fun openInNewWindow(listItemData: DataModel){
+    private fun openInNewWindow(listItemData: DataModel) {
         onListItemClickListener.onItemClick(listItemData)
     }
 
-    interface OnListItemClickListener{
+    interface OnListItemClickListener {
         fun onItemClick(data: DataModel)
     }
 
